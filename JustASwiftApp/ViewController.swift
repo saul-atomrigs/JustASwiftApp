bd9dde6c8f7d32de1dab1a183efd0d42cffbadf8
Saul Lee <saul@atomrigs.io>
2022-09-28 17:14:39 +0900

    Initial Commit

---
+++ JustASwiftApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JustASwiftApp
+//
+//  Created by 아톰릭스랩 on 2022/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
