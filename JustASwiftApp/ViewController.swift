7582a03cb107aa54144a456a96cc8d40ae63b7e1
Saul Lee <saul@atomrigs.io>
2022-10-06 15:46:56 +0900

    feat: toggle and resize features

--- JustASwiftApp/ViewController.swift
+++ JustASwiftApp/ViewController.swift
@@ -8,15 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var isZoom = false
+    var imgOn: UIImage?
+    var imgOff: UIImage?
+    
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var btnResize: UIButton!
     @IBOutlet var lblHello: UILabel!
     @IBOutlet var txtName: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        imgOn = UIImage(named: "ic_address_on.png")
+        imgOff = UIImage(named: "ic_address_off.png")
+        
+        imgView.image = imgOn
     }
 
 
+    @IBAction func btnResizeImage(_ sender: UIButton) {
+        let scale: CGFloat = 2.0
+        var newWidth: CGFloat, newHeight: CGFloat
+        
+        if (isZoom) {
+            newWidth = imgView.frame.width/scale
+            newHeight = imgView.frame.height/scale
+            btnResize.setTitle("확대", for: .normal)
+        } else {
+            newWidth = imgView.frame.width*scale
+            newHeight = imgView.frame.height*scale
+            btnResize.setTitle("축소", for: .normal)
+        }
+        imgView.frame.size = CGSize(width: newWidth, height: newHeight)
+        isZoom = !isZoom
+    }
+    
+    @IBAction func switchImageOnOff(_ sender: UISwitch) {
+        if sender.isOn {
+            imgView.image = imgOn
+        } else {
+            imgView.image = imgOff
+        }
+    }
     @IBAction func btnSend(_ sender: UIButton) {
         lblHello.text = "Hello, " + txtName.text!
     }
